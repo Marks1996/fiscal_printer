13b5651890f0412d09460ca2b359d3d98c14fe36
Marks1996 <75466900@qq.com>
2023-09-28 10:20:59 +0800

    ✨ feat: 导出rch axon

--- lib/fiscal_printer.dart
+++ lib/fiscal_printer.dart
@@ -2,3 +2,5 @@ library fiscal_printer;
 
 export './printer/custom_xml_http.dart';
 export './printer/espon_xml_http.dart';
+export './printer/axon_sf20_http.dart';
+export './printer/rch_xml_http.dart';
